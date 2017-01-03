a33866485bb69d783e28b3607f960ac48e6cfdd1
ahnhojung <ahnhojung@10.250.66.189>
2017-01-03 18:05:26 +0900

    test

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -23,6 +23,7 @@ public class SimpleServlet extends HttpServlet {
         response.setContentType("text/html");
         response.getWriter().print("Hello Wooga World!<br>");
         response.getWriter().print("Deploy by toolchain..!!");
+        response.getWriter().print("Deploy by toolchain..!!");
     }
 
 }
